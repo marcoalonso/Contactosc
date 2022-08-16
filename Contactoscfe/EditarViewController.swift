27e5d81ff9a1f30624b788107c86ad50ccac95d4
Marco Alonso <marco@garagecoders.net>
2022-08-16 13:51:12 -0500

    Empezando

---
+++ Contactoscfe/EditarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditarViewController.swift
+//  Contactoscfe
+//
+//  Created by marco rodriguez on 16/08/22.
+//
+
+import UIKit
+
+class EditarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
